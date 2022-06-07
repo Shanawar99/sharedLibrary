28921ce0ed16dc45bd0bdd18782ee8801642c9ae
Shanawar <shanawar680@hotmail.com>
2022-06-08 03:01:10 +0500

    check external ip

--- vars/k8sHelper.groovy
+++ vars/k8sHelper.groovy
@@ -4,7 +4,7 @@ def createNamespace(Map configNameSpace) {
 
 def deployRelease(Map configRelease) {
     sh "cd ./kubernetes/helm/k8s && helm upgrade --install -f ./values.yaml ${configRelease.RELEASE_NAME} --set=image.repository=${configRelease.REPOSITORY_URI} --set=image.tag=${configRelease.IMAGE_TAG} --namespace ${configRelease.NAMESPACE} . "
-    sh "kubectl get services --namespace ${configRelease.NAMESPACE}"
+    sh "kubectl get services --namespace ${configRelease.NAMESPACE} -o jsonpath='{.items[*].status.loadBalancer.ingress[*].hostname}'> ~/ips.txt"
     sh '''#!/bin/bash 
     while [[ -z "$(kubectl get svc -n ''' + configRelease.NAMESPACE + ''' -o jsonpath='{.items[*].status.loadBalancer.ingress[*].hostname}')"  ]]; do
     echo "Waiting for external Ip"
