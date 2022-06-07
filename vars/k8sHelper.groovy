6bfd84f1c791ab478a0cf222a529cc9084048761
Shanawar <shanawar680@hotmail.com>
2022-06-08 02:47:07 +0500

    check external ip

--- vars/k8sHelper.groovy
+++ vars/k8sHelper.groovy
@@ -4,9 +4,7 @@ def createNamespace(Map configNameSpace) {
 
 def deployRelease(Map configRelease) {
     sh "cd ./kubernetes/helm/k8s && helm upgrade --install -f ./values.yaml ${configRelease.RELEASE_NAME} --set=image.repository=${configRelease.REPOSITORY_URI} --set=image.tag=${configRelease.IMAGE_TAG} --namespace ${configRelease.NAMESPACE} . "
-  //  sh "kubectl get services --namespace ${configRelease.NAMESPACE}"
-    sh "kubectl get svc -n ''' + configRelease.NAMESPACE + ''' -o jsonpath='{.items[*].status.loadBalancer.ingress[*].hostname}'"
-
+    sh "kubectl get services --namespace ${configRelease.NAMESPACE}"
     sh '''#!/bin/bash 
     kubectl get svc -n ''' + configRelease.NAMESPACE + ''' -o jsonpath='{.items[*].status.loadBalancer.ingress[*].hostname}'
     while [[ -z "$(kubectl get svc -n ''' + configRelease.NAMESPACE + ''' -o jsonpath='{.items[*].status.loadBalancer.ingress[*].hostname}')"  ]]; do
