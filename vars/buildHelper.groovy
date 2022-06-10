b0ebbc18116f510beec6e2e755b6752df23b3918
Shanawar <shanawar680@hotmail.com>
2022-06-10 22:25:13 +0500

    added --kubeconfig

--- vars/buildHelper.groovy
+++ vars/buildHelper.groovy
@@ -2,5 +2,5 @@ def buildMaven() {
     sh './mvnw -Dmaven.test.failure.ignore=true clean package'
 }
 def buildDockerImage(Map config) {
-    sh "docker build -t ${config.RELEASE_NAME}:${config.IMAGE_TAG} ."
+    sh "docker build -t ${config.REPOSITORY_URI}:${config.IMAGE_TAG} ."
 }
