a10ba4d6bcfe36a55f7ee33e0857cb7b7547dfe1
Shanawar <shanawar680@hotmail.com>
2022-06-10 05:02:28 +0500

    added --kubeconfig

--- vars/k8sHelper.groovy
+++ vars/k8sHelper.groovy
@@ -1,5 +1,5 @@
 def createNamespace(Map configNameSpace) {
-    sh "kubectl get namespace --kubeconfig ~/.kube/config_${configNameSpace.CLUSTER_NAME} | grep -o ${configNameSpace.NAMESPACE} || kubectl create namespace --kubeconfig ~/.kube/config_${configNameSpace.CLUSTER_NAME} ${configNameSpace.NAMESPACE}"
+    sh "kubectl get namespace --kubeconfig ~/.kube/config_${configNameSpace.CLUSTER_NAME} | grep -q "^${configNameSpace.NAMESPACE} " || kubectl create namespace --kubeconfig ~/.kube/config_${configNameSpace.CLUSTER_NAME} ${configNameSpace.NAMESPACE}"
 }
 
 def deployRelease(Map configRelease) {
